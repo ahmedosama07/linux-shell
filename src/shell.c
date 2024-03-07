829629d47b1a274d0c119196bf315d06f8055205
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2024-03-07 12:48:45 +0200

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -7,98 +7,13 @@
 #include <signal.h>
 #include <sys/resource.h>
 #include "shell.h"
+#include "strings.h"
 
 char input[ARRAY_SIZE];
 char *parsedInput[ARRAY_SIZE];
 
 int counter = 0, exportFlag = 0, echoFlag = 0, cdFlag = 0, pwdFlag = 0;
 int backGroundIndex = 0, exitFlag = 0;
-
-
-/* Function to replace all occurrences of a substring with another substring */
-char* replace_substring(const char *string, const char *substring, const char *replacement) {
-    int length = strlen(substring);
-    const char *temp = string;
-    int count = 0;
-    
-    // Count the number of occurrences
-    while ((temp = strstr(temp, substring)) != NULL) {
-        temp += length;
-        count++;
-    }
-
-    // Allocate memory for the new string
-    int newLength = strlen(string) + (strlen(replacement) - length) * count + 1;
-    char *newString = (char*)malloc(newLength);
-
-    if (newString == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Copy the original string and perform replacements
-    temp = string;
-    char *current = newString;
-    while (*temp) {
-        if (strstr(temp, substring) == temp) {
-            strcpy(current, replacement);
-            temp += length;
-            current += strlen(replacement);
-        } else {
-            *current++ = *temp++;
-        }
-    }
-    *current = '\0';
-
-    return newString;
-}
-
-// Function to remove preceding white spaces from a string
-char* remove_leading_spaces(char* str) {
-    if (str == NULL) {
-        return NULL; // Return NULL if the input string is NULL
-    }
-    
-    int len = strlen(str);
-    int i, j;
-
-    // Find the index of the first non-space character
-    for (i = 0; i < len; i++) {
-        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\r') {
-            break;
-        }
-    }
-
-    // Shift characters to the left to remove preceding spaces
-    for (j = 0; i < len; i++, j++) {
-        str[j] = str[i];
-    }
-    str[j] = '\0'; // Null-terminate the string at the new length
-
-    return str;
-}
-
-// Function to remove trailing white spaces from a string
-char* remove_trailing_spaces(char* str) {
-    if (str == NULL) {
-        return NULL; // Return NULL if the input string is NULL
-    }
-    
-    int len = strlen(str);
-    int i;
-
-    // Find the index of the last non-space character
-    for (i = len - 1; i >= 0; i--) {
-        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\r') {
-            break;
-        }
-    }
-
-    // Null-terminate the string at the last non-space character
-    str[i + 1] = '\0';
-
-    return str;
-}
 
 /* Function to get input from the user */
 void read_input(void){
@@ -164,30 +79,48 @@ void execute_shell_bultin(void){
         }
         else{
             int flag = 0;
-            flag = chdir(parsedInput[1]) ; 
+			char* dest = replace_substring(parsedInput[1], "~", getenv("HOME"));
+			flag = chdir(dest) ; 
             if( flag != 0){
                 printf("Error, the directory is not found\n");
             }
         }
     }
     else if(exportFlag){
-        char* data = remove_leading_spaces(parsedInput[2]);
-		char* env = remove_trailing_spaces(parsedInput[1]);
-        /* Check for quotation marks */
-        if(data[0] == '"'){
-            data++;
-            data[strlen(data)-1] = '\0';
-			setenv(env, data , 1);
-        }
-        else{
-            setenv(env, data , 1);
-        }
+		extern char **environ; // Declare the environment variables array
+		if (parsedInput[1] == NULL)
+		{
+			for (int i = 0; environ[i] != NULL; i++) {
+				printf("%s\n", environ[i]);
+			}
+		}
+        else
+		{
+			char* env = remove_trailing_spaces(parsedInput[1]);
+			if(parsedInput[2] == NULL) {
+				setenv(env, "", 1);
+			}
+			else{
+				char* data = remove_leading_spaces(parsedInput[2]);
+				/* Check for quotation marks */
+			if(data[0] == '"'){
+				data++;
+				data[strlen(data)-1] = '\0';
+				setenv(env, data , 1);
+			}
+			else{
+				setenv(env, data , 1);
+			}
+			}
+			
+		}
+		
     }
     else if(echoFlag){
 			int i = 1;
 			for(i = 1; i <= backGroundIndex; i++)
 			{
-				char *echoEnv = parsedInput[i];
+				char *echoEnv = remove_quotes(parsedInput[i]);
 				if(echoEnv[0] == '$'){
                 echoEnv++;
                 char* temp = getenv(echoEnv);
@@ -207,20 +140,17 @@ void execute_shell_bultin(void){
 
 /* Function to execute shell commands */
 void execute_command (void){
-    int status , foregroundId;
-    int errorCommand = 1;
-    int child_id = fork();
-    if(child_id  == -1){
-        printf("System Error!\n");
+	int errorCommand = 0;
+	pid_t pid = fork();
+
+	
+	if (pid < 0) {
+        fprintf(stderr, "Fork failed.\n");
         exit(EXIT_FAILURE);
-    }
-    else if (child_id == 0){
-        if(parsedInput[1] == NULL){
-            errorCommand = execvp(parsedInput[0] , parsedInput);
-        }
-        else if(parsedInput[1] != NULL){
-            char* env = parsedInput[1];
-            if(env[0] == '$'){
+    } else if (pid == 0) {
+        // Child process
+		if((parsedInput[1] != NULL) && (parsedInput[1][0] == '$')){
+				char* env = parsedInput[1];
                 int  i = 1;
                 char*envTemp;
                 env++;
@@ -229,31 +159,28 @@ void execute_command (void){
                 while(exportTemp != NULL){
                 parsedInput[i++] = exportTemp;    
                 exportTemp = strtok(NULL, " ");
-                }
             }
-            errorCommand = execvp(parsedInput[0] , parsedInput);
         }
+		errorCommand = execvp(parsedInput[0] , parsedInput);
         if(errorCommand){
             printf("Error ! unknown command\n");
             exit(EXIT_FAILURE);
         }
-    }
-    else{
-        if(strcmp(parsedInput[backGroundIndex] , "&")==0){
-            return;
+    } else {
+        // Parent process
+		if (strcmp(parsedInput[backGroundIndex] , "&")==0)
+		{
+			printf("PID: %d\n", pid);
+		}
+        else {
+            waitpid(pid, NULL, 0);
         }
-        else{
-            foregroundId = waitpid(child_id , &status , 0);
-            if(foregroundId == -1){
-                perror("Error in waitpad function\n");
-                return;
-            }
-        if(errorCommand){
+
+		if(errorCommand){
             FILE * file = fopen("log.txt" , "a");
             fprintf(file , "%s" , "Child process terminated\n");
             fclose(file);
         }
-      }
     }
 }
 
